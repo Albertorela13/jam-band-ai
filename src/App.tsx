d76f902a52382c7a9188790a40ec8065069a5094
Albertorela13 <albertorela13@gmail.com>
2026-09-22 07:58:26 +0200

    chore: remove unused React Query and legacy toast system

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AppLayout } from "@/components/layout/AppLayout";
 import Index from "./pages/Index.tsx";
@@ -11,29 +9,24 @@ import RunTest from "./pages/RunTest.tsx";
 import TestDetail from "./pages/TestDetail.tsx";
 import HistoryPage from "./pages/HistoryPage.tsx";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <AppLayout>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/persona/new" element={<PersonaEditor />} />
-            <Route path="/persona/:id" element={<PersonaEditor />} />
-            <Route path="/test/new" element={<RunTest />} />
-            <Route path="/test/:id" element={<TestDetail />} />
-            <Route path="/history" element={<HistoryPage />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AppLayout>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <TooltipProvider>
+    <Sonner />
+    <BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/persona/new" element={<PersonaEditor />} />
+          <Route path="/persona/:id" element={<PersonaEditor />} />
+          <Route path="/test/new" element={<RunTest />} />
+          <Route path="/test/:id" element={<TestDetail />} />
+          <Route path="/history" element={<HistoryPage />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
+  </TooltipProvider>
 );
 
 export default App;
